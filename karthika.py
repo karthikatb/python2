2d0722df67dad0d641765d880a5fedfe05aeb381
karthikatb <karthikatb20@gmail.com>
2024-10-04 16:10:23 +0530

    Add files via upload

---
+++ karthika.py
@@ -0,0 +1,55 @@
+#print("hello\nworld")
+#print("hello world")
+'''
+if 5>2:
+ print("five is greater than two!")
+if 5>2:
+    print("five is greater than two!")
+if 5>2:
+       print("five greater than 2!)")
+'''
+'''
+x=5
+x="lisa"
+print(x)
+'''
+'''
+x=int(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+print(x,y,z)
+x=5.1
+y="abc"
+print(type(x))
+print(type(y))
+'''
+'''
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+'''
+'''
+x="pyton"
+y="is"
+z="awesome"
+print(x+y+z)
+'''
+'''
+username=input("enter user name:")
+print("user name is:"+username)
+'''
+'''
+a="hello"
+b="world"
+c=a+b
+print(c)
+'''
+a="hello,world"
+print(a[3])
+
+b="hello,world"
+print(b[:5])
