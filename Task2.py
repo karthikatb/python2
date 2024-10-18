add90f7952c794c81cced3a52d86d7355358503a
karthikatb <karthikatb20@gmail.com>
2024-10-18 15:10:16 +0530

    task2

---
+++ Task2.py
@@ -0,0 +1,8 @@
+'''
+author:karthika tb
+date:18-10-2024
+description:python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_var="Hello, World!"
+str_another_var=str_var[7:12]
+print(str_another_var+" Everyone!")
