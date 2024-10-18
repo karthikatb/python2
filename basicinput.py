dd0ad64a5785c32e069d64a01b33d683c706a76d
karthikatb <karthikatb20@gmail.com>
2024-10-18 14:29:14 +0530

    basicinputfile

---
+++ basicinput.py
@@ -0,0 +1,9 @@
+'''
+author:karthika tb
+date:18-10-2024
+description:Python program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age.
+'''
+name=input("enter your name:")
+age=input("enter your age:")
+print("hello,",name,"you are",age,"years old"
