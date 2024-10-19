921647e3002db14c8c9d0c9cf684b56d33f09b40
karthikatb <karthikatb20@gmail.com>
2024-10-19 14:47:52 +0530

    arithmeticoperator

---
+++ arithmeticoperators.py
@@ -0,0 +1,13 @@
+'''
+author:karthika tb
+date :19-10=2024
+'''
+number1=int(input("enter first number"))
+number2=int(input("enter a second number"))
+number3=int(input("enter a third number"))
+print("The sum of num1 and num2 is:",number1+number2)
+print("The difference when num2 is substracted from num1 is:",number2-number1)
+print("The product of num1 and num2 is:",number1*number2)
+print("The quotient when num1 is divided by num2 is:",number1/number2)
+print("The remainder when num1 is divided by num2 is:",number1%number2)
+print("The result of (num1+num2)*num3/2is:",(number1+number2)*number3/2)
