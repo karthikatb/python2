8b554545b3ce684ad7782971dad911d02a245d1e
karthikatb <karthikatb20@gmail.com>
2024-10-25 14:22:50 +0530

    Add files via upload

---
+++ largest_nummber.py
@@ -0,0 +1,10 @@
+'''
+author:karthika tb
+date:25-10-2024
+'''
+number1=int(input("enter first number:"))
+number2=int(input("enter second number:"))
+if number1>number2:
+    print(number1,"is greater than",number2)
+else:
+    print(number2,"is greater than",number2)
