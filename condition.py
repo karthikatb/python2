42153a31f92a68556e0d441ea63d2aa056dd4620
karthikatb <karthikatb20@gmail.com>
2024-10-25 14:29:59 +0530

    condition

---
+++ condition.py
@@ -0,0 +1,10 @@
+'''
+author:karthika tb
+date:25-20-2024
+description:file to find a number is positive or negative
+'''
+number1=int(input("enter a number:"))
+if number1>0:
+    print(number1, "is positive")
+else:
+    print(number1,"is negative")
