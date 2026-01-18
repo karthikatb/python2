f99e408680b65410ed1d4a6bc8086c2a89f37cda
karthikatb <karthikatb20@gmail.com>
2026-01-18 18:50:25 +0530

    Add files via upload

---
+++ right_triangle.py
@@ -0,0 +1,6 @@
+#Print a star pattern (right-angled triangle)
+num=int(input("Enter the number of rows:"))
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()        
