dfb3059773f0142a245238cf9d1fc34d68af99a9
karthikatb <karthikatb20@gmail.com>
2026-02-01 18:40:37 +0530

    Add files via upload

---
+++ Filtering.py
@@ -0,0 +1,11 @@
+# Create a sample collection
+users={'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+for user,status in users.copy().items():
+    if status=='inactive':
+        del users[user]
+print(users)        
+# Strategy:  Create a new collection
+active_users={}
+for user,status in users.items():
+    if status=='active':
+        active_users[user]=status
